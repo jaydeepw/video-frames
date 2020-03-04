a8f5c52d46b411bfdd98352e6938068452d80905
Jay <jaydeep.w@gmail.com>
2020-03-04 11:16:58 +0530

    Implement permissions in order

--- app/src/main/java/com/trustingsocial/android/MainActivity.kt
+++ app/src/main/java/com/trustingsocial/android/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.trustingsocial.android
 
 import android.Manifest
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -8,17 +9,19 @@ import android.database.Cursor
 import android.media.MediaMetadataRetriever
 import android.net.Uri
 import android.os.Bundle
-import android.os.Handler
 import android.provider.MediaStore
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 import java.io.File
 import java.io.FileInputStream
+import java.util.concurrent.TimeUnit
 
 // content://media/external/video/media/174021
 // /storage/emulated/0/DCIM/Camera/VID_20200304_100219.mp4
@@ -29,59 +32,106 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        /*fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }*/
-
         button.setOnClickListener {
-            captureCamera()
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED
+            ) {
+                Log.w(TAG, "All storage permissions are already given")
+                requestStoragePermission()
+            } else {
+                Log.i(TAG, "All permissions are already given")
+                if (IS_DEVELOPING) {
+                    showFramesFromUri(Uri.parse("content://media/external/video/media/174028"))
+                } else {
+                    captureCamera()
+                }
+            }
         }
+    }
 
-        if (checkSelfPermission(
-                Manifest.permission.READ_EXTERNAL_STORAGE
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            requestPermissions(
-                arrayOf(
-                    Manifest.permission.READ_EXTERNAL_STORAGE,
-                    Manifest.permission.CAMERA
-                ), PERMISSIONS_CODE
-            )
-        }
+    private fun requestStoragePermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ),
+            PERMISSIONS_CODE_STORAGE
+        )
+    }
 
-        if (IS_DEVELOPING) {
-            showFramesFromUri(Uri.parse("content://media/external/video/media/174021"))
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>, grantResults: IntArray
+    ) {
+        when (requestCode) {
+            PERMISSIONS_CODE_STORAGE -> {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.isNotEmpty()
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[1] == PackageManager.PERMISSION_GRANTED
+                ) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    Log.i(TAG, "All permissions are already given")
+                    captureCamera()
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(this, "Some permissions are denied", Toast.LENGTH_LONG).show()
+                }
+                return
+            }
+
+            // Add other 'when' lines to check for other
+            // permissions this app might request.
+            else -> {
+                // Ignore all other requests.
+            }
         }
     }
 
     private fun captureCamera() {
         val captureVideoIntent = Intent(MediaStore.ACTION_VIDEO_CAPTURE)
-        captureVideoIntent.putExtra(MediaStore.EXTRA_DURATION_LIMIT, 3)
+        captureVideoIntent.putExtra(MediaStore.EXTRA_DURATION_LIMIT, getVideoDuration())
         startActivityForResult(captureVideoIntent, VIDEO_CAPTURED)
+    }
+
+    private fun getVideoDuration(): Int {
+        return if (IS_DEVELOPING) {
+            3
+        } else {
+            5
+        }
     }
 
     private fun getRealPathFromURI(context: Context, contentUri: Uri): String? {
         var cursor: Cursor? = null
         return try {
             val proj = arrayOf(MediaStore.Images.Media.DATA)
-            cursor = context.getContentResolver().query(contentUri, proj, null, null, null)
-            val column_index: Int? = cursor?.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+            cursor = context.contentResolver.query(contentUri, proj, null, null, null)
+            val columnIndex: Int? = cursor?.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
             cursor?.moveToFirst()
-            cursor?.getString(column_index!!)
+            cursor?.getString(columnIndex!!)
         } finally {
             cursor?.close()
         }
     }
 
-    val handler = Handler()
-
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        val videoFileUri = data?.data
-        Log.d("M", "videoFileUri: $videoFileUri")
-        showFramesFromUri(videoFileUri)
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == VIDEO_CAPTURED) {
+                val videoFileUri = data?.data
+                Log.d(TAG, "videoFileUri: $videoFileUri")
+                showFramesFromUri(videoFileUri)
+            }
+        } else {
+            Log.d(TAG, "activity cancelled...")
+        }
     }
 
     private fun showFramesFromUri(videoFileUri: Uri?) {
@@ -114,39 +164,9 @@ class MainActivity : AppCompatActivity() {
         retriever.setDataSource(inputStream.fd)
         val time = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION)
         Log.d(TAG, "time: $time")
-        // retriever.frameAtTime
+        val bitmap = retriever.getFrameAtTime(TimeUnit.SECONDS.toMicros(1))
+        Log.d(TAG, "height: ${bitmap.height}")
     }
-
-/*
-    public fun getFileDuration(Context context, File file) : String {
-        String result = null;
-        MediaMetadataRetriever retriever = null
-        FileInputStream inputStream = null
-
-        try {
-            retriever = new MediaMetadataRetriever()
-            inputStream = new FileInputStream(file.getAbsolutePath());
-            retriever.setDataSource(inputStream.getFD());
-            long time = Long.parseLong(retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION));
-            result = String.format(context.getResources().getString(R.string.player_time_format),
-                AppUtil.getPlayerMinutes(time), AppUtil.getPlayerSoconds(time));
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (RuntimeException e) {
-            e.printStackTrace();
-        } finally{
-            if (retriever != null){
-                retriever.release()
-            }if (inputStream != null){
-                inputStream.close()
-            }
-        }
-        return result;
-    }
-*/
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -166,9 +186,9 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
         private const val VIDEO_CAPTURED: Int = 21
-        private const val PERMISSIONS_CODE: Int = 22
+        private const val PERMISSIONS_CODE_STORAGE: Int = 22
         private val TAG = MainActivity::class.java.simpleName
 
-        private const val IS_DEVELOPING = true
+        private const val IS_DEVELOPING = false
     }
 }
